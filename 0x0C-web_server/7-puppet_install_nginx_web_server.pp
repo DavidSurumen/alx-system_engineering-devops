15bb84a9309e4afda8d64a427a92d1b5d18f538a
David Surumen <davidsurumen@gmail.com>
2022-09-24 10:59:39 +0000

    fix

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -11,7 +11,7 @@ file { '/var/www/html/index.nginx-debian.html':
 file_line { 'aaaaa':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
-  after  => 'listen [::]:80 default_server;',
+  after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com permanent;'
 }
 
