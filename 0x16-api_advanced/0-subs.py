ea344ff875109d30dc5ebf016ef22f75ac913a84
David Surumen <davidsurumen@gmail.com>
2023-02-13 12:06:24 +0300

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Queries the Reddit API and returns the number of subscribers for a
+given subreddit."""
+import requests
+from sys import argv
+
+def number_of_subscribers(subreddit):
+    """Get the number of subscribers on a subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    header = requests.utils.default_headers()
+    headers = {'User-Agent': 'Surumens Notebook'}
+    header.update(headers)
+    res = requests.get(url, headers=header).json()
+
+    count = res.get('data', {}).get('subscribers')
+    if not count:
+        return 0
+    return count
+
+
+if __name__ == "__main__":
+    print(number_of_subscribers(argv[1]))
