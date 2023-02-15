bd5ce5db3241c268deba4f4989daf809a36dd2e9
David Surumen <davidsurumen@gmail.com>
2023-02-15 16:03:44 +0300

    puppet lint correction

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fixes apache returning 500, after finding out the issue using strace
 exec { 'fix-apache':
-  command	=> 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
-  path		=> ['/bin', '/usr/bin', '/usr/sbin']
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
