d67ab0b787907013e2f48d81d6fec544dbfa94e1
David Surumen <davidsurumen@gmail.com>
2023-02-15 15:47:05 +0300

    solution

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Fixes apache returning 500, after finding out the issue using strace
+exec { 'fix-apache':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart';
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
