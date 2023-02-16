5a3acb771f899dc30a5ad5c4df9ab65cdc1dcd15
David Surumen <davidsurumen@gmail.com>
2023-02-16 18:31:07 +0300

    using datadog's api to get dashboard id

---
+++ 0x18-webstack_monitoring/example.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Datadog's API
+-----------
+Get all dashboards returns "OK" response.
+
+Usage:  DD_SITE="datadoghq.com" DD_API_KEY="<DD_API_KEY>" DD_APP_KEY="<DD_APP_KEY>" ./example.py
+"""
+
+if __name__ == "__main__":
+    from datadog_api_client import ApiClient, Configuration
+    from datadog_api_client.v1.api.dashboards_api import DashboardsApi
+
+    configuration = Configuration()
+    with ApiClient(configuration) as api_client:
+        api_instance = DashboardsApi(api_client)
+        response = api_instance.list_dashboards(
+                filter_shared=False,
+                )
+
+    print(response)   # print all dashboards.
+    print('id:  ', response.get('dashboards')[0].get('id'))  # print dashboard id of first dashboard
