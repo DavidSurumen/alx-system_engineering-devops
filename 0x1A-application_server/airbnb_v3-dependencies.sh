d58a0e8f1f996dc05f37d71ff3c78dbf8ac2b7ef
David Surumen <davidsurumen@gmail.com>
2023-03-10 10:47:06 +0300

    .

--- 0x1A-application_server/airbnb_v3-dependencies.sh
+++ 0x1A-application_server/airbnb_v3-dependencies.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 # Installs required packages
 sudo pip3 install SQLAlchemy
+
+sudo pip3 install Flask
 sudo pip3 install -U flask_cors
+
+sudo apt-get install python3-dev
+sudo apt-get install libmysqlclient-dev
+sudo apt-get install zlib1g-dev
+sudo pip3 install mysqlclient
